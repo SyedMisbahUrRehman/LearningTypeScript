a13a89d8dc43dcc6ee0a655cb091f1a9f64c7316
syedmisbahurrehman <syedmisbah588@gmail.com>
2023-09-07 19:05:34 +0500

    initial commit

---
+++ StructuralTyping.js
@@ -0,0 +1,12 @@
+"use strict";
+var customer1 = {
+    name: 'Ahmed',
+    age: 20
+};
+var customer2 = {
+    name: 'Musab',
+    age: 69
+};
+customer1 = customer2;
+console.log(customer1);
+//while equating, structure should be same tasweer khenchne wla bol rha h k typing kro xD
---
+++ StructuralTyping.ts
@@ -0,0 +1,14 @@
+let customer1 ={
+    name:'Ahmed',
+    age:20
+}
+let customer2 ={
+    name:'Musab',
+    age:69
+}
+
+customer1=customer2
+
+console.log(customer1)
+
+//while equating, structure should be same tasweer khenchne wla bol rha h k typing kro xD
